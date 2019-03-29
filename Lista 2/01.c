f8139b2ef194df58e491ff71d62aa9393bffac70
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-03-28 23:13:31 -0300

    Questão 01

--- Lista 2/01.c
+++ Lista 2/01.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+    Fazer um programa em C que peça um número inteiro e 
+    imprima uma tabela com a tabuada deste número, 
+    do menor para o maior e depois do maior para o menor.
+*/
 int main(int argc, char *argv[]){
     int numero,contador=1;
     
@@ -9,17 +13,17 @@ int main(int argc, char *argv[]){
     
     printf("\n--- CRESCENTE ---\n\n");
     while(contador<=numero){
-            printf("%d x %d = %d\n",numero,contador,numero*contador);
-            contador++;
-            }
+         printf("%d x %d = %d\n",numero,contador,numero*contador);
+         contador++;
+    }
             
-    contador=numero;
+    contador = numero;
     
     printf("\n--- DECRESCENTE ---\n\n");      
     while(contador>0){
-            printf("%d x %d = %d\n",numero,contador,numero*contador);
-            contador--;
-                            }
+         printf("%d x %d = %d\n",numero,contador,numero*contador);
+         contador--;
+    }
     
     system("pause");    
     return 0;
