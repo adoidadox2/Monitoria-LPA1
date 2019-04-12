3cbbbaa7b4ec068a89cf50c4c289b901e64e4e9c
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-04-12 11:45:12 -0300

    Questão 1 de verdade pow

--- Lista 4/01.c
+++ Lista 4/01.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Faça um programa para criar dois vetores de 10 números inteiros, A e B. O programa deve preencher o vetor A com
-// valores digitados pelo usuário. O vetor B deve ser preenchido da seguinte forma: os elementos de índice par são os correspondentes
-// de A divididos por 2 e os elementos de índice ímpar são os correspondentes de A multiplicados por 3. Ao final, o programa deverá
-// exibir os dois vetores (A e B).
-
 void main(void){
-    int a[10], b[10];
+    int matriculas[10], matricula, posicao, presente = 0;
 
-    // Preenchendo o vetor a com os números informados pelo usuário
+    // Preenchendo o vetor de matriculas com os números informados pelo usuário
     for(int i = 0; i < 10; i++){
-        printf("Informe o valor do indice %d do vetor a[]: ", i);
-        scanf("%d", &a[i]);
+        printf("Informe o numero da matricula %d: ", i + 1);
+        scanf("%d", &matriculas[i]);
     }
 
-    // Preenchendo o vetor b de acordo com os valores do vetor a
+    printf("Informe o numero da matricula: ");
+    scanf("%d", &matricula);
+    
     for(int i = 0; i < 10; i++){
-        // Verificando se o índice é um número par
-        if(i % 2 == 0)
-            // O valor de b no índice i vai ser igual a metade do valor do vetor a no índice i
-            b[i] = a[i] / 2;
-        else
-            // O valor de b no índice i vai ser igual ao triplo do valor do vetar a no índice i
-            b[i] = a[i] * 3;
+        if( matricula == matriculas[i] ){
+            posicao = i;
+            presente = 1;
+            break;
+        }
     }
 
-
-    // Mostrando os valores do vetor b
-    for(int i = 0; i < 10; i++)
-        printf("b[%d] = %d\n", i, b[i]);
+    if(presente){
+        printf("O numero esta na lista, na posicao %d !\n", posicao);
+    }else{
+        printf("O numero nao esta na lista !\n");
+    }
+    
 
     system("pause");
 }
